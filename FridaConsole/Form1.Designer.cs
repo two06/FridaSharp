6272800706f965f0ea7472380f2371aa84a7966e
two06 <email.two06@gmail.com>
2019-06-14 11:33:38 +0100

    Initial commit - add v1

---
+++ FridaConsole/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace FridaConsole
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtProcessName = new System.Windows.Forms.TextBox();
+            this.txtScriptLocation = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnHook = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(217, 65);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtProcessName
+            // 
+            this.txtProcessName.Location = new System.Drawing.Point(100, 12);
+            this.txtProcessName.Name = "txtProcessName";
+            this.txtProcessName.Size = new System.Drawing.Size(192, 20);
+            this.txtProcessName.TabIndex = 1;
+            // 
+            // txtScriptLocation
+            // 
+            this.txtScriptLocation.Location = new System.Drawing.Point(100, 39);
+            this.txtScriptLocation.Name = "txtScriptLocation";
+            this.txtScriptLocation.Size = new System.Drawing.Size(192, 20);
+            this.txtScriptLocation.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Process Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Script Location:";
+            // 
+            // btnHook
+            // 
+            this.btnHook.Location = new System.Drawing.Point(136, 66);
+            this.btnHook.Name = "btnHook";
+            this.btnHook.Size = new System.Drawing.Size(75, 23);
+            this.btnHook.TabIndex = 5;
+            this.btnHook.Text = "Hook";
+            this.btnHook.UseVisualStyleBackColor = true;
+            this.btnHook.Click += new System.EventHandler(this.btnHook_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 101);
+            this.Controls.Add(this.btnHook);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtScriptLocation);
+            this.Controls.Add(this.txtProcessName);
+            this.Controls.Add(this.btnExit);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Frida Console";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtProcessName;
+        private System.Windows.Forms.TextBox txtScriptLocation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnHook;
+    }
+}
+
---
+++ FridaConsole/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace FridaConsole
+{
+    public partial class Form1 : Form
+    {
+        static Frida.DeviceManager deviceManager;
+        static List<Frida.Device> Devices;
+        static List<Frida.Process> Processes;
+        static Frida.Session session;
+        static Frida.Script script;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Hook(string targetProcess, string scriptLocation)
+        {
+            try
+            {
+                Devices = new List<Frida.Device>();
+                Processes = new List<Frida.Process>();
+                deviceManager = new Frida.DeviceManager(Dispatcher.CurrentDispatcher);
+                var devices = deviceManager.EnumerateDevices();
+                //we want the local device
+                var device = devices.Where(x => x.Name == "Local System").FirstOrDefault();
+                if (device == null)
+                {
+                    Console.WriteLine("[*] Failed to select device!");
+                    return;
+                }
+                var processes = device.EnumerateProcesses();
+                foreach (var process in processes)
+                {
+                    Processes.Add(process);
+                }
+                Console.WriteLine(String.Format("[*] Got {0} processes...", Processes.Count));
+
+                //find the process
+                var target = Processes.Where(x => x.Name == targetProcess).FirstOrDefault();
+                if (target == null)
+                {
+                    foreach (Frida.Process p in Processes)
+                    {
+                        Console.WriteLine(p.Name);
+                    }
+                    Console.WriteLine("[*] Failed to identify target process!");
+                    return;
+                }
+                Console.WriteLine(String.Format("[*] Process {0} found...", targetProcess));
+                session = device.Attach(target.Pid);
+                Console.WriteLine(String.Format("[*] Hooked process with PID {0}", target.Pid.ToString()));
+                //Inject the script...
+                Console.WriteLine("[*] Injecting script...");
+                try
+                {
+                    Console.WriteLine(String.Format("[*] Loading script from {0}...", scriptLocation));
+                    string scriptText = File.ReadAllText(scriptLocation);
+                    script = session.CreateScript(scriptText);
+                    Console.WriteLine("[*] Script loaded...");
+                    script.Message += new Frida.ScriptMessageHandler(script_Message);
+                    script.Load();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("[*] Error loading script!");
+                    Console.WriteLine(ex.ToString());
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[*] Fail!");
+            }
+        }
+        private void script_Message(object sender, Frida.ScriptMessageEventArgs e)
+        {
+            if (sender == script)
+            {
+                Console.WriteLine(String.Format("Message from Script: {0}", e.Message));
+                Console.WriteLine(String.Format("Data: {0}", e.Data == null ? "null" : String.Join(", ", e.Data)));
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnHook_Click(object sender, EventArgs e)
+        {
+            string targetProcess = txtProcessName.Text;
+            string scriptLocation = txtScriptLocation.Text;
+
+            Hook(targetProcess, scriptLocation);
+        }
+    }
+}
